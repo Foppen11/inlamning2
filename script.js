13d3e34763b0da00f42ce378cbc2ca8f7ce20c70
Foppen11 <max.forsberg@outlook.com>
2021-01-09 18:08:52 +0100

    new

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 new WOW().init();
+$('.post-wrapper').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    autoplay:true,
+    autoplaySpeed:4000,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
+    responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1,
+          }
+        }
+      ],
+  });
